a12eabe1d912040c9aad4e476766b6685f9d7e61
Qin Liu <qinliu2020@gmail.com>
2024-04-24 19:23:50 -0400

    Support training

--- opencount/core/engine/auto_trainer.py
+++ opencount/core/engine/auto_trainer.py
@@ -4,12 +4,13 @@ import torch
 from torch.utils.data import DataLoader
 from typing import Tuple, Dict
 from tqdm import tqdm
+from torch import distributed as dist
 
 from opencount.core.utils.log import logger, TqdmToLogger, SummaryWriterAvg
 from opencount.core.model.auto_model import autoCountModel
 from opencount.core.utils.optimizer import get_optimizer
 from opencount.core.utils.scheduler import get_scheduler
-from opencount.core.utils.distributed import get_dp_wrapper
+from opencount.core.utils.distributed import get_dp_wrapper, get_sampler
 from opencount.core.loss.focal_loss import FocalLoss
 
 
@@ -28,8 +29,10 @@ class AutoTrainer(object):
         self.is_master = self.cfg.local_rank == 0
 
         self.train_data = DataLoader(
-            trainset,
-            cfg.batch_size,
+            trainset, cfg.batch_size,
+            sampler=get_sampler(trainset, shuffle=True, distributed=True),
+            num_workers=cfg.workers,
+            drop_last=True, pin_memory=True
         )
 
         self.device = cfg.device
@@ -39,7 +42,7 @@ class AutoTrainer(object):
             self.model = get_dp_wrapper()(
                 self.model, 
                 device_ids=[cfg.gpu_ids[cfg.local_rank]],
-                find_unused_parameters=True
+                find_unused_parameters=False
             )
 
         self.model = load_weights(self.model, self.cfg.weights)
@@ -49,26 +52,12 @@ class AutoTrainer(object):
             for _ in range(cfg.start_epoch):
                 self.sched.step()
 
-        self.lr = optimizer_params['lr']
         self.tqdm_out = TqdmToLogger(logger, level=logging.INFO)
         
         if loss_func_params['name'] == 'Focal':
             class_num = loss_func_params['class_num']
             alpha = loss_func_params['alpha']
             self.loss_func = FocalLoss(class_num, alpha, use_gpu=True)
-
-    def training(self, epoch):
-        self.model.train()
-
-        tbar = tqdm(self.train_data, file=self.tqdm_out, ncols=100) \
-            if self.is_master else self.train_data
-
-        train_loss = 0
-        for i, batch_data in enumerate(tbar):
-            global_step = epoch * len(self.train_data) + i
-
-        if self.is_master:
-            pass
 
     def run(self, num_epochs, start_epoch=None, validation=False):
         if start_epoch is None:
@@ -83,8 +72,54 @@ class AutoTrainer(object):
             if validation:
                 self.validation(epoch)
 
+    def training(self, epoch):
+        self.model.train()
+
+        self.train_data.sampler.set_epoch(epoch)
+        tbar = tqdm(self.train_data, file=self.tqdm_out, ncols=100) \
+            if self.is_master else self.train_data
+
+        for i, batch_data in enumerate(tbar):
+            global_step = epoch * len(self.train_data) + i
+
+            loss = self.batch_forward(batch_data)
+
+            self.optim.zero_grad()
+            loss.backward()
+            self.optim.step()
+
+            dist.all_reduce(loss, op=dist.ReduceOp.SUM)
+
+            if self.is_master:
+                loss /= dist.get_world_size()
+                lr = self.sched.get_lr()[-1]
+                tbar.set_description(
+                    f'Epoch {epoch}, training loss {loss.item():.4f}, lr {lr:.4f}'
+                )
+
     def validation(self, epoch):
         pass
+
+    def batch_forward(self, batch_data, validation=False):
+
+        with torch.set_grad_enabled(not validation):
+            crops, masks, frames, filenames = batch_data
+            crops, masks = crops.to(self.device), masks.to(self.device)
+
+            outputs = self.model(crops)
+            outputs = outputs.permute(0, 2, 3, 1).contiguous()
+            outputs = outputs.view(-1, outputs.shape[-1])
+
+            masks = masks.permute(0, 2, 3, 1).contiguous()
+            masks = masks.view(-1, masks.shape[-1])
+
+            selected_sample_indices = torch.nonzero(masks[:, 0] >= 0).squeeze()
+            masks = torch.index_select(masks, 0, selected_sample_indices)
+            outputs = torch.index_select(outputs, 0, selected_sample_indices)
+
+            train_loss = self.loss_func(outputs, masks)
+
+        return train_loss
 
 
 def load_weights(model: autoCountModel, weights_path: str) -> autoCountModel:
--- run_train.sh
+++ run_train.sh
@@ -1,6 +1,6 @@
 MODEL_CONFIG=./opencount/models/auto/unet_2048x2448_ecDNA.py
-torchrun --nproc-per-node=4 \
+torchrun --nproc-per-node=3 \
 	     --master-port 29504 \
 	     ./opencount/train.py ${MODEL_CONFIG} \
-	     --batch-size=12 \
-	     --gpus=0,1,2,3
+	     --batch-size=4 \
+	     --gpus=1,2,3
