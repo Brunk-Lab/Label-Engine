fc3c0561e3e6ce4cc1532dc97e0c663b4275f765
Qin Liu <qinliu19@biag-gpu6.cs.unc.edu>
2024-05-15 18:20:24 -0400

    Add datasets

--- engine/core/utils/exp_imports/default.py
+++ engine/core/utils/exp_imports/default.py
@@ -3,8 +3,8 @@ from typing import Tuple, Dict
 from easydict import EasyDict as edict
 
 from engine.core.data.ecDNA import ecDNADataset
-from engine.core.engine.auto_trainer import AutoTrainer
-from engine.core.engine.icount_trainer import ICTrainer
+from engine.core.trainer.auto_trainer import AutoTrainer
+from engine.core.trainer.icount_trainer import ICTrainer
 from engine.core.model.auto_unet_model import UNet
 from engine.core.model.icount_unet_model import iUNet
 from engine.core.utils.normalizer import FixedNormalizer, AdaptiveNormalizer
